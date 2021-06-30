01cef43f5a0c6749d9e093e55b61e642aed3709e
Jon P Smith <jon.smith@selectiveanalytics.com>
2021-06-30 14:20:52 +0100

    Added unit tests for issue #6

--- Test/UnitTests/DataLayerTests/TestExampleDbContext.cs
+++ Test/UnitTests/DataLayerTests/TestExampleDbContext.cs
@@ -1,8 +1,11 @@
 ﻿// Copyright (c) 2019 Jon P Smith, GitHub: JonPSmith, web: http://www.thereformedprogrammer.net/
 // Licensed under MIT license. See License.txt in the project root for license information.
 
+using System;
 using System.Linq;
+using System.Threading.Tasks;
 using DataLayer;
+using EntityClasses;
 using Test.EfHelpers;
 using TestSupport.EfHelpers;
 using Xunit;
@@ -27,6 +30,42 @@ namespace Test.UnitTests.DataLayerTests
                 //VERIFY
                 context.TaxRates.Count().ShouldEqual(2);
                 context.ProductStocks.Count().ShouldEqual(3);
+            }
+        }
+
+        [Fact]
+        public void TestCheckSaveChangesReturningCount()
+        {
+            //SETUP
+            var options = SqliteInMemory.CreateOptions<ExampleDbContext>();
+            using (var context = new ExampleDbContext(options))
+            {
+                context.Database.EnsureCreated();
+
+                //ATTEMPT
+                context.Add(new TaxRate(DateTime.UtcNow, 4));
+                var numUpdates = context.SaveChanges();
+
+                //VERIFY
+                numUpdates.ShouldEqual(1);
+            }
+        }
+
+        [Fact]
+        public async Task TestCheckSaveChangesAsyncReturningCount()
+        {
+            //SETUP
+            var options = SqliteInMemory.CreateOptions<ExampleDbContext>();
+            using (var context = new ExampleDbContext(options))
+            {
+                context.Database.EnsureCreated();
+
+                //ATTEMPT
+                context.Add(new TaxRate(DateTime.UtcNow, 4));
+                var numUpdates = await context.SaveChangesAsync();
+
+                //VERIFY
+                numUpdates.ShouldEqual(1);
             }
         }
 
